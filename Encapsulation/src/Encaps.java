4e444fceedde35dcdbbc8a2030ec44669b04b65b
HARI <HARI@DESKTOP-TGPGN4M>
2022-07-24 20:58:45 +0530

    Encapsulation commit

---
+++ Encapsulation/src/Encaps.java
@@ -0,0 +1,10 @@
+
+public class Encaps {
+	public static void main(String[] args) {
+		Project a = new Project();
+		System.out.println(a.getName());
+		System.out.println(a.getAge());
+		System.out.println(a.getTech());
+	}
+
+}
---
+++ Encapsulation/src/Project.java
@@ -0,0 +1,31 @@
+
+public class Project {
+	private String name ="Ananth";
+	private int age = 24;
+	String tech = "java";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	
+	
+}
+	
+//	public String getname() {
+//		return name;
+//	}
+//}
